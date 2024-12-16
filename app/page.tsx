87009105ccf5a7b1fb886493d1a84a22a1a4a4b0
Vitaliy Kupriyanov <kupriyanovv07.dev@gmail.com>
2024-12-16 23:47:12 +0300

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import { Categories, Container, Title } from '@/components/shared'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-	return <h1>Главная</h1>
+	return (
+		<>
+			<Container className='mt-10'>
+				<Title text='Все пиццы' size='lg' className='font-extrabold' />
+
+				<Categories />
+			</Container>
+		</>
+	)
 }
---
+++ components/shared/index.ts
@@ -0,0 +1,4 @@
+export { Container } from './container'
+export { Header } from './header'
+export { Categories } from './categories'
+export { Title } from './title'
